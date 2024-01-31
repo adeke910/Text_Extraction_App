b467af4cfee2ee210096daf22c298cebbeeb05d7
adeke910 <adekemibamiteko@gmail.com>
2024-01-31 21:15:04 +0100

    First working stage

--- OCR_APP/app.py
+++ OCR_APP/app.py
@@ -1,8 +1,138 @@
-# from app import app
-from flask import Flask
-from app.views import my_view
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+import numpy as np
+import cv2
+import pytesseract
+import string
+import random
+import os
+import csv
+import json
+import fitz  # PyMuPDF
+# from utils import extract_text_from_image, remove_characters, save_to_csv, save_to_json
 
-app = Flask(__name__)
-app.register_blueprint(my_view)
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+# # Adding path to config
+app.config['INITIAL_FILE_UPLOAD'] = 'static/uploads'
+
+# Function to extract text from an image using Tesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+def extract_text_from_image(image):
+    custom_config = r'-l eng --oem 3 --psm 6'
+    text = pytesseract.image_to_string(image, config=custom_config)
+    return text
+
+# Function to remove specified characters from a string
+
+
+def remove_characters(text, characters):
+    for character in characters:
+        text = text.replace(character, "")
+    return text
+
+# Function to save extracted text to a CSV file
+
+
+def save_to_csv(data, filename='output.csv'):
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['Text'])
+        csv_writer.writerows([[text] for text in data])
+
+# Function to save extracted text to a JSON file
+
+
+def save_to_json(data, filename='output.json'):
+    with open(filename, 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, ensure_ascii=False, indent=2)
+
+
+def extract_text_from_pdf(pdf_file):
+    text = ''
+    try:
+        pdf_document = fitz.open(pdf_file)
+        for page_number in range(pdf_document.page_count):
+            page = pdf_document[page_number]
+            text += page.get_text()
+        pdf_document.close()
+    except Exception as e:
+        return f'Error extracting text from PDF: {str(e)}'
+    return text
+
+# Route to home page
+
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+@app.route("/", methods=["GET", "POST"])
+def process_image():
+    # Execute if request is get
+    if request.method == "GET":
+        full_filename = 'images/white_bg.jpg'
+        return render_template("index.html", full_filename=full_filename)
+
+    if request.method == "POST":
+        file_upload = request.files['file_upload']
+        file_name = file_upload.filename
+
+        # check the file extension
+        _, file_extension = os.path.splitext(file_name)
+
+        if file_extension.lower() in ['.jpg', '.jpeg', '.png', '.gif']:
+            # process image file
+            image = Image.open(file_upload)
+
+            # Converting image to array
+            image_arr = np.array(image.convert('RGB'))
+
+            # Converting image to grayscale
+            gray_img_arr = cv2.cvtColor(image_arr, cv2.COLOR_BGR2GRAY)
+            # Converting image back to RGB
+            image = Image.fromarray(gray_img_arr)
+
+            # Printing lowercase
+            letters = string.ascii_lowercase
+
+            # Generating unique image name for dynamic image display
+            name = ''.join(random.choice(letters) for i in range(10)) + '.png'
+            full_filename = 'uploads/' + name
+
+            # Extracting text from image
+            text = extract_text_from_image(image)
+
+            # Remove specified symbols
+            characters_to_remove = "!()@—*“>+-/,'|£#%$&^_~"
+            cleaned_text = remove_characters(text, characters_to_remove)
+
+            # Converting string into a list to display extracted text in separate lines
+            extracted_text_list = cleaned_text.split("\n")
+
+        elif file_extension.lower == '.pdf':
+            # process PDF file
+            extracted_text_list = extract_text_from_pdf(file_upload)
+
+        # Save extracted text to CSV or JSON based on user's choice
+        save_option = request.form.get('save_option')
+
+        if save_option == 'csv':
+            save_to_csv(extracted_text_list, filename='output.csv')
+        elif save_option == 'json':
+            save_to_json(extracted_text_list, filename='output.json')
+
+        # # Saving image to display in HTML
+        # img = Image.fromarray(image_arr, 'RGB')
+        # img.save(os.path.join(app.config['INITIAL_FILE_UPLOAD'], name))
+
+        # Returning template, filename, extracted text
+        return render_template('index.html', full_filename=full_filename, text=extracted_text_list)
+
+
 if __name__ == '__main__':
     app.run(debug=False)
--- OCR_APP/app/__init__.py
+++
@@ -1,9 +0,0 @@
-# from app import app
-from flask import Flask
-
-app = Flask(__name__)
-
-app.config.from_object("config.DevelopmentConfig")
-
-
-app.run(debug=True)
